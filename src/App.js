246f18d0ef90b04f56a6fb25b991388a036da686
MilHouse10 <alstjr_96@naver.com>
2021-06-15 16:21:24 +0900

    thunk를 통한 웹 요청

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
+import * as postActions from './modules/post';
 
 class App extends Component {
+  
+  componentDidMount(){
+    //컴포넌트가 처음 마운트 될 때 현재 number를 postId로 사용하여 포스트 내용을 불러온다.
+    const { number, PostActions } = this.props;
+    PostActions.getPost(number);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { PostActions } = this.props;
+
+    //현재 number와 새로 받을 number가 다를 경우에 요청을 시도.
+    if(this.props.number !== nextProps.number){
+      PostActions.getPost(nextProps.number)
+    }
+  }
+
   render() {
-    const { CounterActions, number } = this.props;
+    const { CounterActions, number, post, error, loading } = this.props;
 
     return(
       <div>
-        <h1>{number}</h1>
-        <button onClick={CounterActions.incrementAsync}>+</button>
-        <button onClick={CounterActions.decrementAsync}>-</button>
+        <p>{number}</p>
+        <button onClick={CounterActions.increment}>+</button>
+        <button onClick={CounterActions.decrement}>-</button>
+        { loading && <h2>로딩중...</h2>}
+        { error
+          ? <h1>에러 발생!</h1>
+          : (
+              <div>
+                <h1>{post.title}</h1>
+                <p>{post.title}</p>
+              </div>
+            )}
       </div>
     );
   }
@@ -19,9 +45,13 @@ class App extends Component {
 
 export default connect(
   (state) => ({
-    number: state.counter
+    number: state.counter,
+    post: state.post.data,
+    loading: state.post.pending,
+    error: state.post.error
   }),
   (dispatch) => ({
-    CounterActions: bindActionCreators(counterActions, dispatch)
+    CounterActions: bindActionCreators(counterActions, dispatch),
+    PostActions: bindActionCreators(postActions, dispatch)
   })
 )(App);
